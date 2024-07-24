a809c2e184c3db5322ee6ab51a1c0e7145196d73
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-07-24 16:46:49 +0800

    Employee workshift code clean up

--- resources/js/Pages/Timekeeping/EmployeeWorkSchedule.vue
+++ resources/js/Pages/Timekeeping/EmployeeWorkSchedule.vue
@@ -384,22 +384,16 @@
                                 </label>
                                 <label v-if="setType !== 'Edit'" class="w-full inline-flex items-center cursor-pointer pt-2 pl-2 border-t">
                                     <input
-
                                     type="checkbox" @change="setToAll('shift',$event)"
                                     class="sr-only peer"
-                                    :checked="setType === 'Edit'"
-
                                     />
                                     <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                                     <span class="ms-3 text-sm font-medium text-gray-700">Set for the remaining unassigned days in the period</span>
                                 </label>
                                 <label class="w-full inline-flex items-center cursor-pointer pt-2 pl-2 border-t">
                                     <input
-
                                     type="checkbox" @change="setToAll('emp',$event)"
                                     class="sr-only peer"
-                                    :checked="setType === 'Edit'"
-
                                     />
                                     <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                                     <span class="ms-3 text-sm font-medium text-gray-700">Assign to all Employee</span>
