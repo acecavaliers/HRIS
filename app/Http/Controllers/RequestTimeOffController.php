8ce5da817f5378db6dd845556e2d095dab78c1bd
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-09-03 16:24:27 +0800

    update requesttimeoff controller store

--- app/Http/Controllers/RequestTimeOffController.php
+++ app/Http/Controllers/RequestTimeOffController.php
@@ -29,7 +29,13 @@ class RequestTimeOffController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+
+        $arr = $request->formdata;
+
+        $query = RequestTimeOff::create($arr);
+    }
 
     /**
      * Display the specified resource.
